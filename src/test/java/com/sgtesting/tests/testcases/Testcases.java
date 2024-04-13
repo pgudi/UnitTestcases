98bf412bbc7c44c7bdd83b1d201bcaf2089d475f
pgudi <prabhakargudi@gmail.com>
2024-04-13 12:11:49 +0530

    unit testcases

---
+++ src/test/java/com/sgtesting/tests/testcases/Testcases.java
@@ -0,0 +1,76 @@
+package com.sgtesting.tests.testcases;
+
+import org.testng.annotations.Test;
+
+public class Testcases {
+	
+	@Test
+	public void method1()
+	{
+		System.out.println("It is Method1 testcase Statement");
+	}
+	
+	@Test
+	public void method2()
+	{
+		System.out.println("It is Method2 testcase Statement");
+	}
+	
+	@Test
+	public void method3()
+	{
+		System.out.println("It is Method3 testcase Statement");
+	}
+	
+	@Test
+	public void method4()
+	{
+		System.out.println("It is Method4 testcase Statement");
+	}
+	
+	@Test
+	public void method5()
+	{
+		System.out.println("It is Method5 testcase Statement");
+	}
+	
+	@Test
+	public void method6()
+	{
+		System.out.println("It is Method6 testcase Statement");
+	}
+	
+	
+	@Test
+	public void method7()
+	{
+		System.out.println("It is Method7 testcase Statement");
+	}
+	
+	
+	@Test
+	public void method8()
+	{
+		System.out.println("It is Method8 testcase Statement");
+	}
+	
+	@Test
+	public void method9()
+	{
+		System.out.println("It is Method9 testcase Statement");
+	}
+	
+	@Test
+	public void method10()
+	{
+		System.out.println("It is Method10 testcase Statement");
+	}
+	
+	@Test
+	public void method11()
+	{
+		System.out.println("It is Method11 testcase Statement");
+	}
+	
+
+}
